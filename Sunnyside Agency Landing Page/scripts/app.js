7b1dbf115f18adc04c25d68eab03e92f13f98255
Ahnaf Hasan Shifat <81911439+ah-naf@users.noreply.github.com>
2021-07-28 21:00:19 +0600

    Delete Sunnyside Agency Landing Page directory

--- Sunnyside Agency Landing Page/scripts/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-    $('.ham').on('click', () => {
-        if ($('#navbar').css('transform') === 'matrix(0, 0, 0, 0, 0, 0)') {
-            $('#navbar').css('transform', 'scale(1,1)');
-        } else {
-            $('#navbar').css('transform', 'scale(0,0)');
-        }
-    });
-});
